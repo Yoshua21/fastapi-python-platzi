13474f56e282634624c7141f5f94803e497fc156
Bryan Yoshua <byab_bimy@hotmail.com>
2023-11-06 14:26:13 -0600

    Se agrego para ponrele nombre y version y numero asi como un HTML modulo para mandar codigo HTML

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app=FastAPI() #se crea una instancia de fastAPI
+app.title ="Aplicacion con FasAPI " #Se coloca nobre a la documentacion
+app.version="0.0.1" #Se coloca la version que se esta viendo
 
-@app.get('/')
+@app.get('/',tags=['home'])#se crea la ruta y se le conoca una etiqueta
 def messaege():
-    return "Duncan se la come"
+    return HTMLResponse('<h1>"Duncan de la come"</h1>')
