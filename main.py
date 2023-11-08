8cccae98ac027484b582430cf5064741f80b2487
Bryan Yoshua <byab_bimy@hotmail.com>
2023-11-08 17:48:41 -0600

    SE actualizo el codigo al global y se añadio el metodo de generacion de token asi como usuario para email y contraseña

--- main.py
+++ main.py
@@ -1,103 +1,100 @@
 from fastapi import FastAPI, Body, Path, Query
-from fastapi.responses import HTMLResponse
-from pydantic import BaseModel,Field
-from typing import Optional
+from fastapi.responses import HTMLResponse, JSONResponse
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from jwt_manager import create_token
+
+app = FastAPI()
+app.title = "Mi aplicación con  FastAPI"
+app.version = "0.0.1"
+
+class User(BaseModel):
+    email:str
+    password:str
 
 class Movie(BaseModel):
     id: Optional[int] = None
     title: str = Field(min_length=5, max_length=15)
     overview: str = Field(min_length=15, max_length=50)
     year: int = Field(le=2022)
-    rating: float = Field(ge=0.0, le=10.0)
-    category: str = Field(min_length=5, max_length=15)
+    rating:float = Field(ge=1, le=10)
+    category:str = Field(min_length=5, max_length=15)
 
     class Config:
         schema_extra = {
             "example": {
                 "id": 1,
-                "title": "Shrek",
-                "overview": "Ogro timido encuentra amor inesperado.",
-                "year": 2001,
-                "rating": 7.8,
-                "category": "Comedia"
+                "title": "Mi película",
+                "overview": "Descripción de la película",
+                "year": 2022,
+                "rating": 9.8,
+                "category" : "Acción"
             }
         }
 
 movies = [
     {
-        'id': 1,
-        'title': 'Avatar',
-        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
-        'rating': 7.8,
-        'category': 'ACCION'    
-    },
-      {
-        'id':2,
-        'title': "moana",
-        'overview': "PElicula de disney ...",
-        'year': '20222',
-        'rating': 9,
-        'category': 'Animada'    
-    } ,
-      {
-        'id':3,
-        'title': "shuek",
-        'overview': "PElicula de disney ...",
-        'year': '2017',
-        'rating': 7,
-        'category': 'Animada'    
-    } 
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	},
+    {
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
 ]
 
-app=FastAPI() #se crea una instancia de fastAPI
-app.title ="Aplicacion con FasAPI " #Se coloca nobre a la documentacion
-app.version="0.0.1" #Se coloca la version que se esta viendo
+@app.get('/', tags=['home'])
+def message():
+    return HTMLResponse('<h1>Hello world</h1>')
 
-@app.get('/',tags=['home'])#se crea la ruta y se le conoca una etiqueta
-def messaege():
-    return HTMLResponse('<h1>Duncan de la come</h1>')
 
-@app.get('/movies',tags=['movies'])
-def get_movies():
-    return movies
+@app.post('/login', tags=['auth'])
+def login(user: User):
+    return user
 
-@app.get('/movies/{id}',tags=['movies']) #Se creo un api donde se espera a que se envie un parametro en la ruta para poder repsonder de acuerdo al parametro
-def get_movie(id:int=Path(ge=1,le=2000)):
+@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200)
+def get_movies() -> List[Movie]:
+    return JSONResponse(status_code=200, content=movies)
+
+@app.get('/movies/{id}', tags=['movies'], response_model=Movie)
+def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
     for item in movies:
-        if item['id']==id:
-            return item
-    return []
+        if item["id"] == id:
+            return JSONResponse(content=item)
+    return JSONResponse(status_code=404, content=[])
 
-@app.get('/movies/',tags=['movies'])
-def get_movie_by_category(category:str=Query(min_length=5,max_length=15)):
-    for tipo in movies:
-        if tipo['category']==category:
-            return tipo
-    return []
+@app.get('/movies/', tags=['movies'], response_model=List[Movie])
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
+    data = [ item for item in movies if item['category'] == category ]
+    return JSONResponse(content=data)
 
-@app.post('/movies',tags=['movies'])
-def create_movie(Movis: Movie):
-    movies.append(Movis)
-    return movies
-    
+@app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
+def create_movie(movie: Movie) -> dict:
+    movies.append(movie)
+    return JSONResponse(status_code=201, content={"message": "Se ha registrado la película"})
 
-@app.put('/movies/{id}',tags=['movies'])
-def update_movie(id: int,movie:Movie):
-    for item in movies:
-        if item['id'] == id:
-            item['title']= Movie.title
-            item['overview']= Movie.overview
-            item['year']= Movie.year
-            item['rating']= Movie.rating
-            item['category']= Movie.category    
-            return movies
-        return "Pelicula no encontrada" 
-    
-@app.delete('/movies/{id}',tags =['movies'])
-def delte_movie(id: int):
+@app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
+def update_movie(id: int, movie: Movie)-> dict:
+	for item in movies:
+		if item["id"] == id:
+			item['title'] = movie.title
+			item['overview'] = movie.overview
+			item['year'] = movie.year
+			item['rating'] = movie.rating
+			item['category'] = movie.category
+			return JSONResponse(status_code=200, content={"message": "Se ha modificado la película"})
+
+@app.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
+def delete_movie(id: int)-> dict:
     for item in movies:
         if item["id"] == id:
             movies.remove(item)
-            return movies
-        return "No localizado"
+            return JSONResponse(status_code=200, content={"message": "Se ha eliminado la película"})
