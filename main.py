756d724e20b6c882c1e0d914911acb5c36feb07e
Bryan Yoshua <byab_bimy@hotmail.com>
2023-11-08 16:12:04 -0600

    se creo un objeto clase movie y se eliminaron los parametros query de algunos metodos

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel,Field
+from typing import Optional
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=15, max_length=50)
+    year: int = Field(le=2022)
+    rating: float = Field(ge=0.0, le=10.0)
+    category: str = Field(min_length=5, max_length=15)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "title": "Shrek",
+                "overview": "Ogro timido encuentra amor inesperado.",
+                "year": 2001,
+                "rating": 7.8,
+                "category": "Comedia"
+            }
+        }
 
 movies = [
     {
@@ -56,33 +78,27 @@ def get_movie_by_category(category:str,year:int):
     return []
 
 @app.post('/movies',tags=['movies'])
-def create_movie(id: int= Body(), title:str=Body(), overview: str=Body(), year: int=Body(), rating: float=Body(), category: str=Body()):
-    movies.append({
-        "id":id,
-        'title': title,
-        'overview': overview,
-        'year': year,
-        'rating': rating,
-        'category': category    
-    })
+def create_movie(Movis: Movie):
+    movies.append(Movis)
+    return movies
     
 
 @app.put('/movies/{id}',tags=['movies'])
-def update_movie(id: int, title:str=Body(), overview: str=Body(), year: int=Body(), rating: float=Body(), category: str=Body()):
+def update_movie(id: int,movie:Movie):
     for item in movies:
-        if item['id']==id:
-            item['title']= title
-            item['overview']= overview
-            item['year']= year
-            item['rating']= rating
-            item['category']= category    
+        if item['id'] == id:
+            item['title']= Movie.title
+            item['overview']= Movie.overview
+            item['year']= Movie.year
+            item['rating']= Movie.rating
+            item['category']= Movie.category    
             return movies
         return "Pelicula no encontrada" 
     
 @app.delete('/movies/{id}',tags =['movies'])
 def delte_movie(id: int):
     for item in movies:
-        if item["id"]==id:
+        if item["id"] == id:
             movies.remove(item)
             return movies
         return "No localizado"
